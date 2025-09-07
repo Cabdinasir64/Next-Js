144fefdb2a7e972f4d753c09b4877fbe8a4222fb
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-07 09:00:28 +0300

    fix: replace div with motion.div for enhanced animation effects on Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="bg-pink-400 min-h-screen flex flex-col items-center justify-center">
+    <motion.div className="bg-pink-400 min-h-screen flex flex-col items-center justify-center"
+      initial={{ opacity: 0, }}
+      animate={{ opacity: 1, }}
+      transition={{ duration: 0.8, delay: 0.1 }}
+    >
       <motion.h1
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, delay: 0.6 }} 
+        transition={{ duration: 0.8, delay: 0.6 }}
         className="text-5xl font-bold text-white"
       >
         Welcome to Next.js!
       </motion.h1>
-    </div>
+    </motion.div>
   );
 };
 
