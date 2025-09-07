db0bbaf2219e4bb9d9574a494e9c99f508c788e2
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-07 08:58:43 +0300

    fix: update Page component to use motion effects for enhanced animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className='bg-pink-400 min-h-screen flex items-center justify-center'>
-      <h1 className='text-5xl font-bold text-white'>Welcome to Next.js!</h1>
+    <div className="bg-pink-400 min-h-screen flex flex-col items-center justify-center">
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.6 }} 
+        className="text-5xl font-bold text-white"
+      >
+        Welcome to Next.js!
+      </motion.h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
